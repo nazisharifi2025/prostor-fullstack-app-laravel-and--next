96c573aa4ebc96fd40cdf13c9d87a7f7a1bd6abe
nazi sharifi <banonazisharifi@gmail.com>
2026-04-09 21:17:01 +0430

    add table cartItem

---
+++ database/factories/CartIemFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\cartIem;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<cartIem>
+ */
+class CartIemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
