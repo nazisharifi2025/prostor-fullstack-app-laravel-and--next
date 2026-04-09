45bb87541e30bd3537db11fb0a836d0c9310369b
nazi sharifi <banonazisharifi@gmail.com>
2026-04-09 21:13:44 +0430

    add table cart

---
+++ database/factories/CartFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\cart;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<cart>
+ */
+class CartFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
