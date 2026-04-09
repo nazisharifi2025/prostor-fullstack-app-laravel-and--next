cddd01b1923dbe3a074c6e45351b59027c911f5c
nazi sharifi <banonazisharifi@gmail.com>
2026-04-09 21:16:09 +0430

    add table reviows

---
+++ database/factories/ReviowFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\reviow;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<reviow>
+ */
+class ReviowFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
