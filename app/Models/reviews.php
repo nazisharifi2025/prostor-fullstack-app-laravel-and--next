f78c4427a95a1f15ec75026ce0ff745eed473f9a
nazi sharifi <banonazisharifi@gmail.com>
2026-04-12 12:17:22 +0430

    add new project

--- app/Models/reviews.php
+++ app/Models/reviews.php
@@ -9,4 +9,15 @@ class reviews extends Model
 {
     /** @use HasFactory<\Database\Factories\ReviewsFactory> */
     use HasFactory;
+    protected $fillable = [
+        "rating",
+        "product_id",
+        "user_id"
+    ];
+    public function products(){
+        return $this->belongsTo(products::class , "product_id");    
+    }
+     public function users(){
+        return $this->belongsTo(User::class , "user_id");    
+    }
 }
