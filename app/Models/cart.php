727df7c93eb00eb15d485f0c843fc7dfd3f1f4fe
nazi sharifi <banonazisharifi@gmail.com>
2026-04-12 12:21:34 +0430

    add new project

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -9,4 +9,15 @@ class cart extends Model
 {
     /** @use HasFactory<\Database\Factories\CartFactory> */
     use HasFactory;
+    protected $fillable = [
+        "cartItem_id",
+        "user_id"
+    ];
+    public function cartItems(){
+        return $this->belongsTo(cartItem::class , "cartItem;_id");    
+    }
+    public function users(){
+        return $this->belongsTo(User::class , "user_id");
+    }
+        
 }
